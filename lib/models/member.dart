06a8ba4bee531f6215353fff182f8243cf1f6ebf
misoangel <46087537+misoangel@users.noreply.github.com>
2026-04-21 15:32:00 +0900

    Update member.dart

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,17 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-enum MemberRole { 위원장, 부위원장, 위원 }
-enum Party { 국민의힘, 더불어민주당, 무소속, 기타 }
+enum MemberRole { chair, viceChair, member }
+enum Party { ppp, dp, independent, other }
+
+extension MemberRoleLabel on MemberRole {
+  String get label {
+    switch (this) {
+      case MemberRole.chair: return '위원장';
+      case MemberRole.viceChair: return '부위원장';
+      case MemberRole.member: return '위원';
+    }
+  }
+}
+
+extension PartyLabel on Party {
+  String get label {
+    switch (this) {
+      case Party.ppp: return '국민의힘';
+      case Party.dp: return '더불어민주당';
+      case Party.independent: return '무소속';
+      case Party.other: return '기타';
+    }
+  }
+}
 
 class Member {
   final String id;
   final String name;
-  final String district;     // 선거구
+  final String district;
   final MemberRole role;
   final Party party;
-  final DateTime termStart;  // 임기 시작
-  final DateTime termEnd;    // 임기 종료
-  final bool isActive;       // 현직 여부
+  final DateTime termStart;
+  final DateTime termEnd;
+  final bool isActive;
 
   Member({
     required this.id,
@@ -32,11 +53,11 @@ class Member {
       district: data['district'] ?? '',
       role: MemberRole.values.firstWhere(
         (e) => e.name == data['role'],
-        orElse: () => MemberRole.위원,
+        orElse: () => MemberRole.member,
       ),
       party: Party.values.firstWhere(
         (e) => e.name == data['party'],
-        orElse: () => Party.무소속,
+        orElse: () => Party.independent,
       ),
       termStart: (data['termStart'] as Timestamp).toDate(),
       termEnd: (data['termEnd'] as Timestamp).toDate(),
